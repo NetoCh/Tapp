a34bcfd3e8318c08a30b46c8dcc1171c66bd164f
RJosue <raul56480@gmail.com>
2020-07-19 23:55:59 -0500

    home Services Fix

--- src/app/services/home.js
+++ src/app/services/home.js
@@ -1,16 +1,23 @@
 'use strict'
 
+const { response } = require('express');
+
 const pool = require('../models/pool')();
 
 function HomeServices() {
-    this.getDestacados = () => {
-        return new Promise((resolve) => {
-            pool.query('CALL pa_traer_destacados()', (error, rows) => {
-                if (error) resolve(error);
-                resolve(rows)
+    try {
+        this.getDestacados = () => {
+            return new Promise((resolve) => {
+                pool.query('CALL pa_traer_destacados()', (error, rows) => {
+                    if (error) resolve(error);
+                    resolve(rows)
+                })
             })
-        })
+        }
+    } catch (ex) {
+        resolve(ex)
     }
+
 }
 
 module.exports = new HomeServices();
